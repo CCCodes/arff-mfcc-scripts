02b7b9848704e939c40fe7424f31f7cf70f46308
Caitlin Chou <caitlinchou@gmail.com>
2017-07-26 13:59:03 -0400

    add stats

--- accumulate.py
+++ accumulate.py
@@ -1,22 +1,22 @@
-import sys
 import os
-import glob
 import re
 from collections import OrderedDict
-import statistics
+
+import numpy as np
+from scipy import stats
 
 os.chdir(os.getenv("HOME"))
 print(os.getcwd())
 
 # path of output arff file = arff_path
-arff_path = 'Documents/arff_results/end_result.arff'
+arff_path = 'PycharmProjects/arff_scripts/arff_results/end_result.arff'
 out_file = open(arff_path, 'w+')
 
 # dir in which we have mfcc's of all audio files
-search_path = 'Documents/arff_samples/'
+search_path = 'PycharmProjects/arff_scripts/arff_samples/'
 # os.chdir(search_path)
 
-arff_base_path = 'Documents/assimilated_arff_base.txt'
+arff_base_path = 'PycharmProjects/arff_scripts/assimilated_arff_base.txt'
 
 with open(arff_base_path, 'r+') as arff_base:
     out_file.write(arff_base.read())
@@ -36,46 +36,42 @@ for root, dir, files in os.walk(search_path):
         print("file_type=%s" % file_type)
         speaker_no = speakers[file_type]  # classify by nominal rather than numeric?
         text = f.read()
-        # pattern=re.compile(r'^(.*@data)(.*)$',re.S)
-        # data=re.sub(pattern,r'\2',text)
         data = text[text.index("@data") + 6:]  # 6 characters in @data\n
 
+        all_data = data.split('\n')
         data_list = []
 
-        for line in data.split('\n'):
-            # print("linelength=%d" % line)
+        for line in all_data:
             data_list.append([float(n) for n in line.split(',')[3:]])  # ignore first three elements
 
-        for i in range(12):
-            print(i)
-            mfcc = [n[i] for n in data_list if len(n) != 0]
-            mfcc_data = OrderedDict({
-                "max": max(mfcc),
-                "min": min(mfcc),
-                "range": max(mfcc) - min(mfcc),
-                "maxPos": mfcc.index(max(mfcc)),
-                "minPos": mfcc.index(min(mfcc)),
-                "amean": statistics.mean(mfcc),
-                "linregc1": "",
-                "linregc2": "",
-                "linregerrA": "",
-                "linregerrQ": "",
-                "stddev": "",
-                "skewness": "",
-                "kurtosis": "",
-                "quartile1": "",
-                "quartile2": "",
-                "quartile3": "",
-                "iqr1-2": "",
-                "iqr2-3": "",
-                "iqr1-3": "",
-            })
-            print(mfcc_data)
+        frame_times = [float(line.split(',')[1]) for line in all_data if len(line) > 2]
 
-        # data=data.split(',')
-        # data=','.join(data[40:268])
-        # out_file.write(data+","+str(speaker_no)+'\n')
+        for i in range(12):
+            mfcc = [n[i] for n in data_list if len(n) != 0]
+
+            mfcc_data = OrderedDict()
+            mfcc_data["max"] = max(mfcc)
+            mfcc_data["min"] = min(mfcc)
+            mfcc_data["range"] = mfcc_data["max"] - mfcc_data["min"]
+            mfcc_data["maxPos"] = mfcc.index(mfcc_data["max"])
+            mfcc_data["minPos"] = mfcc.index(mfcc_data["min"])
+            mfcc_data["amean"] = np.average(mfcc)
+            mfcc_data["stddev"] = np.std(mfcc)
+
+            np_mfcc_arr = np.array(mfcc)
+            mfcc_data["skewness"] = stats.stats.skew(np_mfcc_arr)
+            mfcc_data["kurtosis"] = stats.stats.kurtosis(np_mfcc_arr)
+            mfcc_data["quartile1"] = np.percentile(mfcc, 25)
+            mfcc_data["quartile2"] = np.percentile(mfcc, 50)
+            mfcc_data["quartile3"] = np.percentile(mfcc, 75)
+            mfcc_data["iqr1-2"] = mfcc_data["quartile2"] - mfcc_data["quartile1"]
+            mfcc_data["iqr2-3"] = mfcc_data["quartile3"] - mfcc_data["quartile2"]
+            mfcc_data["iqr1-3"] = mfcc_data["quartile3"] - mfcc_data["quartile1"]
+
+            out_file.write(",".join([str(i[1]) for i in mfcc_data.items()])+",")
+
+        out_file.write(str(speaker_no)+"\n")
+
         f.close()
-        break
 
 out_file.close()
